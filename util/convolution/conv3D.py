55aaa3761a0b62cc5f41b240bceba15a98646848
Nietz Huang <rock19910423@gmail.com>
2021-08-22 13:59:54 +0800

    Add padding variable to conv3D.py pattern generator.

--- util/convolution/conv3D.py
+++ util/convolution/conv3D.py
@@ -1,72 +1,114 @@
 import os
 import numpy as np
 
-def conv3D(filter, image, stride):
+def conv3D(filter, image, stride, padding):
     channel_number, filter_height, filter_width = filter.shape
     channel_number, image_height, image_width = image.shape
-    ofmap_height = int((image_height - filter_height + stride) / stride)
-    ofmap_width = int((image_width - filter_width + stride) / stride)
+    ofmap_height = int((image_height - filter_height + padding*2 + stride) / stride)
+    ofmap_width = int((image_width - filter_width + padding*2 + stride) / stride)
+
+    if padding > 0:
+        image_pad = np.zeros((channel_number, image_height+padding*2, image_width+padding*2))
+        image_pad[:, padding:-padding, padding:-padding] = image
 
     psum = np.zeros((channel_number, ofmap_height, ofmap_width))
     for ch in range(channel_number):
         for i in range(ofmap_height):
             for j in range(ofmap_width):
-                ofpsum = np.multiply(filter[ch, :, :], image[ch, 0+i*stride:filter_height+i*stride, 0+j*stride:filter_width+j*stride])
+                if padding > 0:
+                    ofpsum = np.multiply(filter[ch, :, :], image_pad[ch, 0+i*stride:filter_height+i*stride, 0+j*stride:filter_width+j*stride])
+                else:
+                    ofpsum = np.multiply(filter[ch, :, :], image[ch, 0+i*stride:filter_height+i*stride, 0+j*stride:filter_width+j*stride])
                 psum[ch ,i, j] =  sum(sum(ofpsum))
-                ofmap = sum(psum)
+    ofmap = sum(psum)
 
     return ofmap, psum
 
 # Set parameters
-pattern_name    = 'filter11x11x3_ifmap161x161x3_stride2'
-channels        = 3
-filter_height   = 11
-filter_width    = 11
-ifmap_height    = 161
-ifmap_width     = 161
-stride          = 2
-ofmap_height    = int((ifmap_height - filter_height + stride) / stride)
-ofmap_width     = int((ifmap_width - filter_width + stride) / stride)
+pattern_name    = 'filter3x3x1_ifmap5x5x1_pad1'
+channels        = 1
+filter_num      = 1
+filter_height   = 3
+filter_width    = 3
+ifmap_height    = 5
+ifmap_width     = 5
+stride          = 1
+padding         = 2
+ofmap_height    = int((ifmap_height - filter_height + padding*2 + stride) / stride)
+ofmap_width     = int((ifmap_width - filter_width + padding*2 + stride) / stride)
+ofmap_channels  = filter_num
+
+# Define filename
+filename_filter = "filter_" + str(pattern_name) + ".dat"
+filename_filter_C = "filter_" + str(pattern_name) + "_C++.dat"
+filename_image  = "image_" + str(pattern_name) + ".dat"
+filename_image_C = "image_" + str(pattern_name) + "_C++.dat"
+filename_ofmap = "ofmap_" + str(pattern_name) + ".dat"
+filename_ofmap_C = "ofmap_" + str(pattern_name) + "_C++.dat"
+filename_psum = "psum_" + str(pattern_name) + ".dat"
+filename_psum_C = "psum_" + str(pattern_name) + "_C++.dat"
 
 
 os.chdir('Patterns')
 filter = np.random.randint(0, 2, size=(channels, filter_height, filter_width)) # channel, height, width
 image = np.random.randint(0, 2, size=(channels, ifmap_height, ifmap_width))
-ofmap, psum = conv3D(filter, image, stride=stride)
+ofmap, psum = conv3D(filter, image, stride=stride, padding=padding)
 
-filename_filter = "filter_random" + str(pattern_name) + ".dat"
-filename_filter_C = "filter_rand" + str(pattern_name) + "_C++.dat"
-filename_image  = "image_random" + str(pattern_name) + ".dat"
-filename_image_C = "image_rand" + str(pattern_name) + "_C++.dat"
-filename_ofmap = "ofmap_random" + str(pattern_name) + ".dat"
-filename_ofmap_C = "ofmap_rand" + str(pattern_name) + "_C++.dat"
-# filename_psum = "psum_random" + str(pattern_name) + ".dat"
-filename_psum_C = "psum_rand" + str(pattern_name) + "_C++.dat"
 
-# Write random array data
-filter = np.reshape(filter, (channels, filter_height*filter_width))
+# Write data in shape
+with open(filename_filter, 'w') as f:
+    f.write("Channel: 0\n")
+    f.write("\n\n\nChannel: \n".join(map(str, filter)))
+f.close()
+
+with open(filename_image, 'w') as im:
+    im.write("Channel\n")
+    im.write("\n\n\nChannel\n".join(map(str, image)))
+im.close()
+
+with open(filename_ofmap, 'w') as om:
+    #om.write("Channel\n")
+    #om.write("\n\n\nChannel\n".join(map(str, ofmap)))
+    om.write(str(ofmap))
+om.close()
+
+with open(filename_psum, 'w') as p:
+    p.write("ofmap number: 0\n")
+    p.write("\n\n\nofmap number: \n".join(map(str, psum)))
+p.close()
+
+
+# Write as SystemC pattern
+filter = np.reshape(filter, (channels, filter_height*filter_width*filter_num))
 filter = np.rot90(filter, 3)
 filter = np.fliplr(filter)
-with open(filename_filter_C, 'w') as f:
-    np.savetxt(f, filter, fmt='%d')
+f = open(filename_filter_C, 'w')
+np.savetxt(f, filter, fmt='%d')
 f.close()
 
 image = np.reshape(image, (channels, ifmap_height*ifmap_width))
 image = np.rot90(image, 3)
 image = np.fliplr(image)
-with open(filename_image_C, 'w') as im:
-    np.savetxt(im, image, fmt='%d')
+im = open(filename_image_C, 'w')
+np.savetxt(im, image, fmt='%d')
 im.close()
 
-ofmap = np.reshape(ofmap, (ofmap_height*ofmap_width))
-np.savetxt(filename_ofmap, ofmap, fmt='%d')
-np.savetxt(filename_ofmap_C, ofmap, fmt='%d')  # channel of ofmap is always only one.
+ofmap_tmp = np.zeros((ofmap_channels, ofmap_height*ofmap_width))
+ofmap = np.reshape(ofmap, (ofmap_channels*ofmap_height*ofmap_width))
+for ch in range(ofmap_channels):  # Arrange it in shape as next feature map
+    ofmap_tmp[ch][:] = ofmap[ch*ofmap_height*ofmap_width : (ch+1)*ofmap_height*ofmap_width]
+ofmap = np.rot90(ofmap_tmp, 3)
+ofmap = np.fliplr(ofmap)
+om = open(filename_ofmap_C, 'w')
+np.savetxt(om, ofmap, fmt='%d')
+om.close()
 
-psum = np.reshape(psum, (channels, ofmap_height*ofmap_width))
-psum = np.rot90(psum, 3)
-psum = np.fliplr(psum)
-with open(filename_psum_C, 'w') as p:
-    np.savetxt(p, psum, fmt='%d')  # Save psum in terms of psum and channel in row versus column.
+psum_tmp = np.zeros((ofmap_height*ofmap_width*filter_num, channels))
+psum = np.reshape(psum, (filter_num, channels, ofmap_height*ofmap_width))
+for fil_num in range(filter_num): # Arrange it as the shape row, coloumn in terms of psum data, channels
+    psum_tmp[fil_num*ofmap_height*ofmap_width:(fil_num+1)*ofmap_height*ofmap_width][:] = psum[fil_num][:][:].T
+p = open(filename_psum_C, 'w')
+np.savetxt(p, psum_tmp, fmt='%d')  # Save psum in terms of psum and channel in row versus column.
 p.close()
 
 
