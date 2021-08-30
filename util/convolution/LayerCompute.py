3aec925ff35edb5f8fdb9c99b42b740083c268ba
JarvisMAC <nietzhuang@JarvisMac.local>
2021-08-30 15:08:39 +0800

    Add fully-connected pattern generator

--- util/convolution/LayerCompute.py
+++ util/convolution/LayerCompute.py
@@ -74,22 +74,40 @@ def maxpool(pool_size, image, stride, padding):
 
     return ofmap, psum
 
+def dense(units, weight, image):
+    channel_number, image_height, image_width = image.shape
+    weight_unit, _, _, _ = weight.shape
+    image = np.reshape(image, (channel_number*image_height*image_width))
+    print(image)
+    print('\n')
+    weight = np.reshape(weight, (units, channel_number*image_height*image_width))
+    print(weight)
+
+    output = np.zeros((units, 1))
+    for unit in range(units):
+        for idx in range(channel_number*image_height*image_width):
+            print(image[idx])
+            print(weight[unit, idx])
+            output[unit] += image[idx] * weight[unit, idx]
+
+    return output
 
 
 
 
 
 # Set parameters
-pattern_name                = 'max'
+pattern_name                = 'fc_test'
 dataflow                    = 'RS'
-layer                       = 'MAX'
-channels                    = 2
+layer                       = 'FC'
+channels                    = 3
 filter_num                  = 1
 filter_height, filter_width = (3, 3)
-ifmap_height, ifmap_width   = (6, 6)
+ifmap_height, ifmap_width   = (3, 3)
 pool_height, pool_width     = (2, 2)
 stride                      = 2
 padding                     = 0
+units                       = 5
 if layer == 'CONV':
     ofmap_height            = int((ifmap_height - filter_height + padding*2 + stride) / stride)
     ofmap_width             = int((ifmap_width - filter_width + padding*2 + stride) / stride)
@@ -97,7 +115,7 @@ if layer == 'CONV':
 elif layer == 'MAX':
     ofmap_height            = int((ifmap_height - pool_height + padding*2 + stride) / stride)
     ofmap_width             = int((ifmap_width - pool_width + padding*2 + stride) / stride)
-    ofmap_channels          = channels 
+    ofmap_channels          = channels
 
 # Define filename
 filename_filter = "filter_" + str(pattern_name) + ".dat"
@@ -117,7 +135,6 @@ os.chdir('Patterns')
 filter = np.random.randint(0, 2, size=(channels, filter_height, filter_width)) # channel, height, width
 image = np.random.randint(0, 2, size=(channels, ifmap_height, ifmap_width))
 ofmap, psum = conv3D(filter, image, stride=stride, padding=padding)
-print(ofmap)
 """
 """
 # 4D Convolution
@@ -125,14 +142,18 @@ filter = np.random.randint(0, 2, size=(channels, filter_num, filter_height, filt
 image = np.random.randint(0, 2, size=(channels, ifmap_height, ifmap_width))
 ofmap, psum = conv4D(filter, image, stride=stride, padding=padding)
 """
-
+"""
 # Maxpooling
 filter = np.zeros((channels, filter_height, filter_width))
 image = np.random.randint(0, 10, size=(channels, ifmap_height, ifmap_width))
 ofmap, psum = maxpool((2,2), image, stride=stride, padding=padding)
-print(psum)
-
-
+"""
+# Fully-connected
+filter = np.random.randint(0, 10, size=(units, channels, ifmap_height, ifmap_width))
+image =  np.random.randint(0, 2, size=(channels, ifmap_height, ifmap_width))
+psum = np.zeros((units, 1))
+ofmap = dense(units, filter, image)
+print(ofmap)
 
 
 
@@ -147,7 +168,7 @@ ifmap_width_bw      = 10
 ifmap_height_bw     = 10
 filter_width_bw     = 4
 filter_height_bw    = 4
-Dataflow            = {'WS': 0, 'IS': 1, 'OS': 2, 'RS': 3}
+Dataflow            = {'ＯS': 0, 'WS': 1, 'IS': 2, 'RS': 3}
 LayerType           = {'CONV': 0, 'MAX': 1, 'FC': 2}
 
 config_reg =  '0' * (64 - layer_bw - dataflow_bw - \
@@ -173,8 +194,13 @@ conf.close()
 
 # Write data in shape
 with open(filename_filter, 'w') as f:
-    f.write("Channel: 0\n")
-    f.write("\n\n\nChannel: \n".join(map(str, filter)))
+    if layer != 'FC':
+        f.write("Channel: 0\n")
+        f.write("\n\n\nChannel: \n".join(map(str, filter)))
+    else:
+        f.write("Perceptron: 0\n")
+        f.write("\n\n\nPerceptron: \n".join(map(str, filter)))
+
 f.close()
 
 with open(filename_image, 'w') as im:
@@ -194,9 +220,15 @@ p.close()
 
 
 # Write as SystemC pattern
-filter = np.reshape(filter, (channels, filter_height*filter_width*filter_num))
-filter = np.rot90(filter, 3)
-filter = np.fliplr(filter)
+if layer != 'FC':
+    filter = np.reshape(filter, (channels, filter_height*filter_width*filter_num))
+    filter = np.rot90(filter, 3)
+    filter = np.fliplr(filter)
+else:
+    filter = np.swapaxes(filter, 0, 1)
+    filter = np.reshape(filter, (channels, units*ifmap_height*ifmap_width))
+    filter = np.rot90(filter, 3)
+    filter = np.fliplr(filter)
 f = open(filename_filter_C, 'w')
 np.savetxt(f, filter, fmt='%d')
 f.close()
@@ -208,23 +240,25 @@ im = open(filename_image_C, 'w')
 np.savetxt(im, image, fmt='%d')
 im.close()
 
-ofmap_tmp = np.zeros((ofmap_channels, ofmap_height*ofmap_width))
-ofmap = np.reshape(ofmap, (ofmap_channels*ofmap_height*ofmap_width))
-for ch in range(ofmap_channels):  # Arrange it in shape as next feature map
-    ofmap_tmp[ch][:] = ofmap[ch*ofmap_height*ofmap_width : (ch+1)*ofmap_height*ofmap_width]
-ofmap = np.rot90(ofmap_tmp, 3)
-ofmap = np.fliplr(ofmap)
+if layer != 'FC':
+    ofmap_tmp = np.zeros((ofmap_channels, ofmap_height*ofmap_width))
+    ofmap = np.reshape(ofmap, (ofmap_channels*ofmap_height*ofmap_width))
+    for ch in range(ofmap_channels):  # Arrange it in shape as next feature map
+        ofmap_tmp[ch][:] = ofmap[ch*ofmap_height*ofmap_width : (ch+1)*ofmap_height*ofmap_width]
+        ofmap = np.rot90(ofmap_tmp, 3)
+        ofmap = np.fliplr(ofmap)
 om = open(filename_ofmap_C, 'w')
 np.savetxt(om, ofmap, fmt='%d')
 om.close()
 
-psum_tmp = np.zeros((ofmap_height*ofmap_width*filter_num, channels))
-psum = np.reshape(psum, (filter_num, channels, ofmap_height*ofmap_width))
-for fil_num in range(filter_num): # Arrange it as the shape row, coloumn in terms of psum data, channels
-    psum_tmp[fil_num*ofmap_height*ofmap_width:(fil_num+1)*ofmap_height*ofmap_width][:] = psum[fil_num][:][:].T
-p = open(filename_psum_C, 'w')
-np.savetxt(p, psum_tmp, fmt='%d')  # Save psum in terms of psum and channel in row versus column.
-p.close()
+if layer != 'FC':
+    psum_tmp = np.zeros((ofmap_height*ofmap_width*filter_num, channels))
+    psum = np.reshape(psum, (filter_num, channels, ofmap_height*ofmap_width))
+    for fil_num in range(filter_num): # Arrange it as the shape row, coloumn in terms of psum data, channels
+        psum_tmp[fil_num*ofmap_height*ofmap_width:(fil_num+1)*ofmap_height*ofmap_width][:] = psum[fil_num][:][:].T
+    p = open(filename_psum_C, 'w')
+    np.savetxt(p, psum_tmp, fmt='%d')  # Save psum in terms of psum and channel in row versus column.
+    p.close()
 
 
 # Arrange data path
@@ -240,10 +274,11 @@ try:
     shutil.move("./"+ filename_ofmap_C, "./Convert2C++/.")
 except:
     os.replace("./"+ filename_ofmap_C, "./Convert2C++/" + filename_ofmap_C)
-try:
-    shutil.move("./"+ filename_psum_C, "./Convert2C++/.")
-except:
-    os.replace("./"+ filename_psum_C, "./Convert2C++/" + filename_psum_C)
+if layer != 'FC':
+    try:
+        shutil.move("./"+ filename_psum_C, "./Convert2C++/.")
+    except:
+        os.replace("./"+ filename_psum_C, "./Convert2C++/" + filename_psum_C)
 try:
     shutil.move("./"+ filename_config, "./Convert2C++/.")
 except:
